61f743c9607f642cd56f48600ddb53e89a44cf51
Fatteme <fatteme.khashei@sotoon.ir>
2023-09-21 22:20:26 +0330

    feat: add infinite scroll component

---
+++ src/components/InfiniteScroll.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+export default function InfiniteScroll({ callback }: { callback: Function }) {
+  const observerTarget = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          callback();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
+  return <div ref={observerTarget}></div>;
+}
