a844a531db0967e8dde29972690dd34de37c2e3c
Fatteme <fatteme.khashei@sotoon.ir>
2023-09-22 15:33:34 +0330

    feat(router): redirect main page to restaurant list page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,18 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 
 import App from '../App';
 import RestaurantList from '../pages/RestaurantList';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
-  },
+    path: '',
 
-  {
-    path: 'restaurants',
-    element: <RestaurantList />
+    element: <App />,
+
+    children: [
+      { path: '/', loader: () => redirect('/restaurants') },
+
+      { path: '/restaurants', element: <RestaurantList /> }
+    ]
   }
 ]);
